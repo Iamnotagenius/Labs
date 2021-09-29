7862827d3b6af7b361ca313413ac76e8ded75114
Iamnotagenius <Iamnotagenius@github.com>
2021-09-29 20:53:03 +0300

    Work is done and documented (for 100-BASE version)

--- Lab2/test.c
+++ Lab2/test.c
@@ -5,7 +5,7 @@
 #include <unistd.h>
 
 int main(int argc, char **argv) {
-	/*
+	
 	uint1024_t x = scan_uint1024(argv[1]);
 	char *op = argv[2];
 	uint1024_t y = scan_uint1024(argv[3]);
@@ -17,11 +17,5 @@ int main(int argc, char **argv) {
 		case '/': z = divide(x, y); break;
 		case '%': z = mod(x, y); break;
 	}
-	*/
-	uint1024_t great = uint1024_from_uint(0);
-	while (1) {
-		printf("%s\n", to_str(great));
-		inc(great);
-	}
-	return 0;
+	printf("%s\n", to_str(z));
 }
