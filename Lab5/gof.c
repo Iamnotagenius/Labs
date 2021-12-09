8e5d5e3d3a42708af326160dc543db1e2d07340b
Iamnotagenius <kazakk9@gmail.com>
2021-12-10 02:55:07 +0300

    4-bit cells (input output)

--- Lab5/gof.c
+++ Lab5/gof.c
@@ -38,10 +38,11 @@ gof_field_t *get_from_bmp(FILE *bmp) {
     PRINT_DEBUG("pixel_array_offset = %X, %dx%d", pixel_array_offset, width, height)
 #endif
 
-    gof_field_t *new_field = malloc(sizeof(gof_field_t) + (width + 31)/32 * 4 * abs(height));
+    gof_field_t *new_field = malloc(sizeof(gof_field_t) + (width + 1)/2 * (height + 2));
     new_field->width = width;
     new_field->height = height;
-    new_field->row_size = (width + 31)/32 * 4;
+    new_field->row_size = (width + 1)/2;
+    memset(new_field->field, 0, new_field->row_size * (height + 2));
 
     short bpp;
     if (header_size >= 40) {
@@ -50,29 +51,27 @@ gof_field_t *get_from_bmp(FILE *bmp) {
     fread(&bpp, sizeof(short), 1, bmp);
     fseek(bmp, pixel_array_offset, SEEK_SET);
 
-    if (bpp > 1) {
-        /* TODO: convert >1-bit bitmaps into 1-bit */
-        int input_row_size = (bpp * width + 31)/32 * 4;
-        char row_buf[input_row_size];
-        unsigned field_buf[new_field->row_size / 4];
-        for (int row = (height - 1) * new_field->row_size; row >= 0; row -= new_field->row_size) {
-            /* packing row in 1-bit format */
-            memset(field_buf, 0, new_field->row_size);
-            fread(row_buf, 1, input_row_size, bmp);
-            int byte = 0;
-            for (int i = 0; i < width; ++i) {
-                unsigned bit = memcmp(row_buf + byte, "\xFF\xFF\xFF\xFF", (bpp + 7) / 8) == 0;
-                bit <<= (-i - 1) & 31;
-                field_buf[i / 32] |= bit;
-                byte += (bpp + 7) / 8;
-            }
-            memcpy(new_field->field + row, field_buf, new_field->row_size);
+    unsigned pixel_mask = (1 << bpp) - 1, bmp_row_size = (bpp * width + 31) / 32 * 4;
+    unsigned char bmp_row[bmp_row_size];
+    char cell_mask = 0x10;
+
+    for (int row = height * new_field->row_size; row >= new_field->row_size; row -= new_field->row_size) {
+        pixel_mask = 1 << (8 - bpp);
+        fread(bmp_row, 1, bmp_row_size, bmp);
+        for (int pixel = 0; pixel < width; ++pixel) {
+            int bits = bpp < 8 ? pixel_mask & bmp_row[(bpp * pixel) / 8]
+                               : memcmp(&bmp_row[(bpp * pixel) / 8], "\xff\xff\xff\xff", bpp / 8) == 0;
+            new_field->field[row + pixel / 2] |= cell_mask & (bits ? 0xFF : 0);
+            cell_mask ^= 0x11; /* changing "half" of byte */
+            pixel_mask >>= bpp;
+            if (!pixel_mask)
+                pixel_mask = 1 << (8 - bpp);
         }
-    }
-    else {
-        for (int row = (height - 1) * new_field->row_size; row >= 0; row -= new_field->row_size) {
-            fread(new_field->field + row, 1, new_field->row_size, bmp);
-        }
+        //printf("field[%d] = ", row);
+        //for (int pixel = 0; pixel < new_field->row_size; ++pixel) {
+        //    printf("%02X ", new_field->field[row + pixel]);
+        //}
+        //printf("\n");
     }
 
     free(full_header);
@@ -97,8 +96,15 @@ void dump_field(gof_field_t *field, FILE *bmp) {
     memcpy(header + 0x36, "\0\0\0\0\xff\xff\xff\0", 8);
     fwrite(header, 1, 62, bmp);
 
-    for (int row = (field->height - 1) * field->row_size; row >= 0; row -= field->row_size) {
-        fwrite(field->field + row, 1, field->row_size, bmp);
+    unsigned int row_size = (field->width + 31) / 32 * 4;
+    unsigned char pixel_row[row_size];
+    for (int row = field->height * field->row_size; row >= field->row_size; row -= field->row_size) {
+        memset(pixel_row, 0, row_size);
+        for (int pixel = 0; pixel < field->row_size; ++pixel) {
+            int bits = field->field[row + pixel] % 14;
+            pixel_row[pixel / 4] |= bits << ((3 - (pixel % 4)) << 1); 
+        }
+        fwrite(pixel_row, 1, row_size, bmp);
     }
 }
 
