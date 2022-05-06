022cba52e62e38b12eeeb5375638f892bb2d735d
Iamnotagenius <kazakk9@gmail.com>
2022-05-07 02:39:26 +0300

    Somewhat compiling allocator

--- C++/Lab5/mem_pool_alloc.hpp
+++ C++/Lab5/mem_pool_alloc.hpp
@@ -1,4 +1,5 @@
 #include <stdexcept>
+#include <memory>
 #include <cstdint>
 #include <cstdlib>
 
@@ -6,60 +7,84 @@ namespace memory {
     template<typename T>
     class pool_allocator {
         private:
-            T* heap;
-            T** free_list;
-            std::size_t size_of_piece;
+            std::shared_ptr<void*> _heap;
+            std::shared_ptr<void**> _free_list;
+            std::size_t _size_of_piece;
         public:
             using pointer = T*;
             using value_type = T;
-            pool_allocator(std::size_t size, std::size_t count) 
-                : heap(std::malloc(size * count * sizeof(T))), 
-                  free_list(static_cast<T**>(heap)), 
-                  size_of_piece(size) {
-                    if (size * sizeof(T) < sizeof(std::size_t)) {
+            template<typename U>
+            struct rebind {
+                using other = pool_allocator<U>;
+            };
+            pool_allocator(std::size_t size = 16, std::size_t count = 1024) 
+                : _heap(std::make_shared<void*>(std::malloc(size * count))), 
+                  _free_list(std::make_shared<void**>(reinterpret_cast<void**>(*_heap))), 
+                  _size_of_piece(size) {
+                    if (size < sizeof(std::size_t)) {
                         throw std::invalid_argument("Piece size must be at least pointer size");
                     }
-                auto end_addr = heap + size * count * sizeof(T);
+                auto end_addr = reinterpret_cast<char*>(*_heap) + size * count;
                 
                 for (std::size_t i = 0; i < count; ++i) {
-                    auto current = heap + i * size * sizeof(T);
-                    auto next = current + size * sizeof(T);
+                    auto current = reinterpret_cast<char*>(*_heap) + i * size;
+                    auto next = current + size;
                     // Store address of next piece in the free memory
-                    *reinterpret_cast<T**>(current) = next < end_addr ? next : nullptr;
+                    *reinterpret_cast<void**>(current) = next < end_addr ? next : nullptr;
                 }
             }
 
-            ~pool_allocator() {
-                std::free(heap);
+            bool operator==(const pool_allocator& rhs) {
+                return _heap == rhs._heap;
+            }
+
+            const std::shared_ptr<void*>& get_heap() const {
+                return _heap;
+            }
+
+            const std::shared_ptr<void**>& get_free_list() const {
+                return _free_list;
+            }
+
+            std::size_t get_block_size() const {
+                return _size_of_piece;
             }
             
+            template<typename U>
+            pool_allocator(const pool_allocator<U>& other) :
+                _heap(other.get_heap()),
+                _free_list(other.get_free_list()),
+                _size_of_piece(other.get_block_size()) {}
+
+
             [[nodiscard]] T* allocate(std::size_t n) {
-                if (n > size_of_piece) {
+                if (n > _size_of_piece) {
                     std::invalid_argument("Can't allocate more than size of piece");
                 }
-                if (*free_list == nullptr) {
+                if (**_free_list == nullptr) {
                     std::bad_alloc();
                 }
 
-                auto allocating = reinterpret_cast<T*>(free_list);
-                free_list = static_cast<T**>(*free_list);
+                auto allocating = reinterpret_cast<void*>(*_free_list);
+                *_free_list = reinterpret_cast<void**>(**_free_list);
 
-                return allocating;
+                return reinterpret_cast<T*>(allocating);
 
             }
+
             void deallocate(T* p, std::size_t) {
                 if (p == nullptr) {
                     return;
                 }
 
-                if (free_list == nullptr) {
-                    free_list = reinterpret_cast<T**>(p);
-                    *free_list = nullptr;
+                if (*_free_list == nullptr) {
+                    *_free_list = reinterpret_cast<void**>(p);
+                    **_free_list = nullptr;
                     return;
                 }
-                T** temp = free_list;
-                free_list = reinterpret_cast<T**>(p);
-                *free_list = reinterpret_cast<T*>(temp);
+                void** temp = *_free_list;
+                *_free_list = reinterpret_cast<void**>(p);
+                **_free_list = reinterpret_cast<void*>(temp);
             }
 
     };
